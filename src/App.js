f36e368f2c0a78802a1f549dfbae49b5aefb837e
minjaeKim <smallkdb@gmail.com>
2022-06-20 22:56:19 +0900

    make simple toDoList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 function App() {
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => {
+		setToDo(event.target.value);
+	};
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (toDo === "") {
+			return;
+		}
+		setToDos((toDos) => [toDo, ...toDos]);
+		setToDo("");
+	};
 	return (
 		<div>
-			<div>Welcome back!!</div>
+			<h1>ToDo App</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange}
+					value={toDo}
+					placeholder="insert your ToDo"
+				></input>
+				<button>insert</button>
+			</form>
+			<ul>
+				{toDos.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
