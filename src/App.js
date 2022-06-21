5cc1634935ebea7a8fdb6396f2ab7408c132a05b
minjaeKim <smallkdb@gmail.com>
2022-06-21 10:18:48 +0900

    make coin app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-	const [toDo, setToDo] = useState("");
-	const [toDos, setToDos] = useState([]);
-	const onChange = (event) => {
-		setToDo(event.target.value);
+	const [loading, setLoading] = useState(true);
+	const [USD, setUSD] = useState();
+	const [coins, setCoins] = useState([]);
+	const [usdToBtc, setUsdToBtc] = useState(0);
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/tickers")
+			.then((res) => res.json())
+			.then((data) => {
+				setCoins(data);
+				setUsdToBtc(data[0].quotes.USD.price.toFixed(2));
+				setLoading(false);
+			});
+	}, []);
+	const onChangeUSD = (e) => {
+		setUSD(e.target.value);
 	};
-	const onSubmit = (event) => {
-		event.preventDefault();
-		if (toDo === "") {
-			return;
-		}
-		setToDos((toDos) => [toDo, ...toDos]);
-		setToDo("");
+	const onSubmitUSD = (e) => {
+		e.preventDefault();
+		setUSD(1);
 	};
 	return (
 		<div>
-			<h1>ToDo App</h1>
-			<form onSubmit={onSubmit}>
+			<h1>Coin App</h1>
+			<form onSubmit={onSubmitUSD}>
 				<input
-					onChange={onChange}
-					value={toDo}
-					placeholder="insert your ToDo"
+					onChange={onChangeUSD}
+					value={USD}
+					placeholder="USD To BTC"
 				></input>
-				<button>insert</button>
 			</form>
-			<ul>
-				{toDos.map((item, index) => (
-					<li key={index}>{item}</li>
-				))}
-			</ul>
+			{loading ? (
+				<div>loading...</div>
+			) : (
+				<div>
+					<div>Today Btc : {usdToBtc} USD</div>
+					<div>
+						{USD ? USD : 1} USD is {((USD ? USD : 1) / usdToBtc).toFixed(10)}{" "}
+						BTC
+					</div>
+					<ul>
+						{coins.map((coin, index) => (
+							<li key={index}>
+								{coin.name} : {coin.quotes.USD.price.toFixed(2)} USD,{" "}
+								{(coin.quotes.USD.price.toFixed(2) / usdToBtc).toFixed(10)} Btc
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
